63cfde865608d11d3e3d3a843fb9427b8f0412f9
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2022-09-20 21:00:41 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,6 @@
 variable "access_key" {
   description = "Access key to AWS console"
 }
-variable "secret_key" {
-  description = "Secret key to AWS console"
-}
-variable "region" {
-  description = "Region of AWS VPC"
-}
 
 variable "bucket_name" {
   description = "(Required) Creates a unique bucket name"
