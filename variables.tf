ac08b38cd79aeff30192826f82ddf00afba1850c
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2022-09-21 23:14:54 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,6 @@
 variable "region" {
   description = "AWS region"
 }
-variable "access_key" {
-  description = "access key used for AWS"
-}
-variable "secret_key" {
-  description = "secret key used for AWS"
-}  
 
 variable "bucket_name" {
   description = "(Required) Creates a unique bucket name"
