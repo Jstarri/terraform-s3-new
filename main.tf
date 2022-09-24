347f0c1d6616384ea417e10ff676cebdb5bfc80d
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2022-09-24 19:47:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = var.region
 }
 
+module "iam" {
+  source  = "terraform-aws-modules/iam/aws"
+  version = "5.4.0"
+
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
