3e257839c54c9fc99858944bca2130ef1ccebdab
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2022-09-24 19:49:57 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,14 @@ variable "region" {
   description = "AWS region"
 }
 
+variable "source" {
+  description = "Terraform module source"
+}
+
+variable "version" {
+  description = "Terraform module version"
+}
+
 variable "bucket_name" {
   description = "(Required) Creates a unique bucket name"
   type        = string
