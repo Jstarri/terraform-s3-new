02f2b1f53f46f35ce8baa6776363ed410df0ed6f
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2022-09-30 23:22:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = var.region
 }
 
+assume_role {
+  role_arn = "arn:aws:iam::436248312882:role/Terraform-Ci"
 
 resource "aws_s3_bucket" "this" {
   bucket = "zali-s3-bucket"
